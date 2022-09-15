71f9a2efae2bd0192cc02f79dfe55f1982938c12
Anju0307 <jropero111@gmail.com>
2022-09-15 23:03:20 +0200

    Ejercicio-3

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let base = parseInt(prompt("Ingrese el numero de la base"));
-let altura = parseInt(prompt("Ingrese el numero de la altura"));
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+let numero3 = parseInt(prompt("Ingrese el tercer numero"));
+let numero4 = parseInt(prompt("Ingrese el cuarto numero"));
 
-let area = base * altura;
+let media = (numero1 + numero2 + numero3 + numero4)/4;
 
-document.getElementById('area').innerHTML = area;
+document.getElementById('media').innerHTML = media;
+
