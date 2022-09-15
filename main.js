f64e0d89ff1b72fbc1ded52776039f0fd0a76846
Anju0307 <jropero111@gmail.com>
2022-09-16 00:28:27 +0200

    Ejercicio-8

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let radio = parseFloat(prompt("Ingrese el radio"));
-let pi = Math.PI;
+let A = parseInt(prompt("ingrese el valor de A"));
+let B = parseInt(prompt("ingrese el valor de B"));
+let C = parseInt(prompt("ingrese el valor de C"));
 
-let volumen = 4 / 3 * pi * Math.pow(radio, 3);
+let operacion = (A + 7 * C)/(B + 2 * A) + 2 * B;
 
-document.getElementById('volumen').innerHTML = volumen;
+document.getElementById('operacion').innerHTML = operacion;
