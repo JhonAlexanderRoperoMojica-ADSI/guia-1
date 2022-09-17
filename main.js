aaa84e6bafcaee25b2395977d8bcc389a188e33c
Anju0307 <jropero111@gmail.com>
2022-09-17 04:10:44 +0200

    Ejercicio-18

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-let nota1 = parseFloat(prompt("Ingrese la primera nota"));
-let nota2 = parseFloat(prompt("Ingrese la segudna nota"));
-let nota3 = parseFloat(prompt("Ingrese la tercera nota"));
-let examen_final = parseFloat(prompt("Ingrese la nota del examen final"));
-let trabajo_final = parseFloat(prompt("Ingrese la nota del trabajo final"));
+let horas_pago = 15300;
+let nombre = prompt("Ingrese el nombre del trabajor");
+let horas = parseFloat(prompt("Ingrese las horas que a trabajado"));
 
-let parciales = ((nota1 + nota2 + nota3) / 3) * 0.55;
-let promedio_fin = examen_final * 0.3;
-let promedio_fin2 = trabajo_final * 0.15;
-let nota_final = (parciales + promedio_fin + promedio_fin2) / 3;
+let pago = horas * horas_pago;
 
-document.getElementById('parciales').innerHTML = parciales;
-document.getElementById('promedio_fin').innerHTML = promedio_fin;
-document.getElementById('promedio_fin2').innerHTML = promedio_fin2;
-document.getElementById('nota_final').innerHTML = nota_final;
+document.write("el pago de :" + nombre + " es de :" + pago);
