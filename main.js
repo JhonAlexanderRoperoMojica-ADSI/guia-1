931880b9afdb87165331b6071de739428fcb354a
Anju0307 <jropero111@gmail.com>
2022-09-17 03:19:01 +0200

    Ejercicio-16

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-let grados = parseFloat(prompt("Ingrese los grados a comvertir"));
+let nota1 = parseFloat(prompt("Ingrese la primera nota"));
+let nota2 = parseFloat(prompt("Ingrese la segudna nota"));
+let nota3 = parseFloat(prompt("Ingrese la tercera nota"));
+let examen_final = parseFloat(prompt("Ingrese la nota del examen final"));
+let trabajo_final = parseFloat(prompt("Ingrese la nota del trabajo final"));
 
-let Fahrenheit = (grados * (9/5)) + 32;
+let parciales = ((nota1 + nota2 + nota3) / 3) * 0.55;
+let promedio_fin = examen_final * 0.3;
+let promedio_fin2 = trabajo_final * 0.15;
+let nota_final = (parciales + promedio_fin + promedio_fin2) / 3;
 
-document.getElementById('Fahrenheit').innerHTML = Fahrenheit;
+document.getElementById('parciales').innerHTML = parciales;
+document.getElementById('promedio_fin').innerHTML = promedio_fin;
+document.getElementById('promedio_fin2').innerHTML = promedio_fin2;
+document.getElementById('nota_final').innerHTML = nota_final;
